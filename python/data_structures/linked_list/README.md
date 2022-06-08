b6fe96b43335e639357fd12362fbdc56f56afac5
S14mx <sergey.otryshko@gmail.com>
2022-06-07 19:50:45 -0700

    finished code challenge 06

--- python/data_structures/linked_list/README.md
+++ python/data_structures/linked_list/README.md
@@ -16,29 +16,40 @@ Node based approach
 ## Methods
 
 `insert`
+
 Arguments: value
 Returns: nothing
 Adds a new node with that value to the head of the list with an O(1) Time performance.
+
 `includes`
+
 Arguments: value
 Returns: Boolean
 Indicates whether that value exists as a Node’s value somewhere within the list.
+
 `to string`
+
 Arguments: none
 Returns: a string representing all the values in the Linked List, formatted as:
 "{ a } -> { b } -> { c } -> NULL"
+
 `append`
+
 Arguments: new value
 Returns: nothing
 Adds a new node with the given value to the end of the list
+
 `insert before`
+
 Arguments: value, new value
 Returns: nothing
 Adds a new node with the given new value immediately before the first node that has the value specified
+
 `insert after`
+
 Arguments: value, new value
 Returns: nothing
-Adds a new node with the given new value immediately after the first node that has the value specified
+Adds a new node with the given new value immediately after the first node that has the value specified  
 
 ![Whiteboard screenshot](https://github.com/S14mx/data-structures-and-algorithms/blob/main/python/data_structures/linked_list/imgs/linked_list_insertions.png "Whiteboard process")
 
--- python/tests/code_challenges/test_linked_list_insertions.py
+++ python/tests/code_challenges/test_linked_list_insertions.py
@@ -1,8 +1,7 @@
 import pytest
-from data_structures.linked_list import LinkedList, TargetError
+from data_structures.linked_list.linked_list import LinkedList, TargetError
 
 
-@pytest.mark.skip("TODO")
 def test_append():
     linked_list = LinkedList()
 
@@ -12,10 +11,10 @@ def test_append():
 
     linked_list.append("cucumber")
 
-    assert str(linked_list) == "{ banana } -> { apple } -> { cucumber } -> NULL"
+    assert str(
+        linked_list) == "{ banana } -> { apple } -> { cucumber } -> NULL"
 
 
-@pytest.mark.skip("TODO")
 def test_insert_before():
     linked_list = LinkedList()
 
@@ -25,10 +24,10 @@ def test_insert_before():
 
     linked_list.insert_before("apple", "cucumber")
 
-    assert str(linked_list) == "{ banana } -> { cucumber } -> { apple } -> NULL"
+    assert str(
+        linked_list) == "{ banana } -> { cucumber } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
 def test_insert_before_first():
     linked_list = LinkedList()
 
@@ -39,7 +38,6 @@ def test_insert_before_first():
     assert str(linked_list) == "{ cucumber } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
 def test_insert_after():
     linked_list = LinkedList()
 
@@ -49,10 +47,11 @@ def test_insert_after():
 
     linked_list.insert_after("banana", "cucumber")
 
-    assert str(linked_list) == "{ banana } -> { cucumber } -> { apple } -> NULL"
+    assert str(
+        linked_list) == "{ banana } -> { cucumber } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_insert_before_empty():
     linked_list = LinkedList()
 
@@ -60,7 +59,7 @@ def test_insert_before_empty():
         linked_list.insert_before("radish", "zucchinni")
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_insert_before_missing():
     linked_list = LinkedList()
 
@@ -70,7 +69,7 @@ def test_insert_before_missing():
         linked_list.insert_before("radish", "zucchinni")
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_insert_after_empty():
     linked_list = LinkedList()
 
@@ -78,7 +77,7 @@ def test_insert_after_empty():
         linked_list.insert_after("radish", "zucchinni")
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_insert_after_missing():
     linked_list = LinkedList()
 
