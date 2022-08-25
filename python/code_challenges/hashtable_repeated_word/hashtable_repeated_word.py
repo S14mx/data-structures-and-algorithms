401a37967d60ff9e7f88870079f4d4840e88562a
S14mx <sergey.otryshko@gmail.com>
2022-08-24 22:52:21 -0700

    work in progress

--- python/code_challenges/hashtable_repeated_word/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word/hashtable_repeated_word.py
@@ -1,14 +1,16 @@
-from python.data_structures.hash_table.hashtable import Hashtable
+from data_structures.hash_table.hashtable import Hashtable
 
 
 def first_repeated_word(string):
-    hash_table = Hashtable
+    hash_table = Hashtable()
     words = string.split()
     for word in words:
-        hash_table.set(word, True)
-        if hash_table.contains(word):
+
+        if hash_table.contains(word.lower()):
             return word or None
+        else:
+            hash_table.set(word.lower(), True)
 
 
 if __name__ == "__main__":
-    print(first_repeated_word("hello there"))
+    print(first_repeated_word("hello there hello"))
--- python/data_structures/hash_table/hashtable.py
+++ python/data_structures/hash_table/hashtable.py
@@ -1,4 +1,3 @@
-from xmlrpc.client import Boolean
 from data_structures.linked_list.linked_list import LinkedList, Node
 
 
