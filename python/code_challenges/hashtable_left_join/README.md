6f8da897ef5d352179e898bffef2ff25547213d3
S14mx <sergey.otryshko@gmail.com>
2022-09-01 21:07:31 -0700

    completed challenge 33

---
+++ python/code_challenges/hashtable_left_join/README.md
@@ -0,0 +1,18 @@
+# Challenge Summary
+
+Implement a simplified LEFT JOIN for 2 Hashmaps.
+
+We collaborated with Marni and Jacob on this challenge.
+
+## Whiteboard Process
+
+![Whiteboard screenshot](https://github.com/S14mx/data-structures-and-algorithms/blob/hashmap-left-join/python/code_challenges/hashtable_left_join/imgs/hashtable_left_join.jpg "Whiteboard process")
+
+## Approach & Efficiency
+
+Time: O(n)
+Space O(n)
+
+## Solution
+
+- [*Link to code*](/python/code_challenges/hashtable_left_join/hashtable_left_join.py)
--- python/tests/code_challenges/test_hashtable_left_join.py
+++ python/tests/code_challenges/test_hashtable_left_join.py
@@ -1,36 +1,83 @@
 import pytest
-from code_challenges.hashtable_left_join import left_join
+from code_challenges.hashtable_left_join.hashtable_left_join import left_join
+from data_structures.hash_table.hashtable import Hashtable
 
 
 def test_exists():
     assert left_join
 
 
-@pytest.mark.skip("TODO")
 def test_example():
-    synonyms = {
-        "diligent": "employed",
-        "fond": "enamored",
-        "guide": "usher",
-        "outfit": "garb",
-        "wrath": "anger",
-    }
-    antonyms = {
-        "diligent": "idle",
-        "fond": "averse",
-        "guide": "follow",
-        "flow": "jam",
-        "wrath": "delight",
-    }
+    synonyms = Hashtable()
+    synonyms.set("diligent", "employed")
+    synonyms.set("fond", "enamored")
+    synonyms.set("guide", "usher")
+    synonyms.set("outfit", "garb")
+    synonyms.set("wrath", "anger")
+
+    antonyms = Hashtable()
+    antonyms.set("diligent", "idle")
+    antonyms.set("fond", "averse")
+    antonyms.set("guide", "follow")
+    antonyms.set("flow", "jam")
+    antonyms.set("wrath", "delight")
 
     expected = [
-        ["fond", "enamored", "averse"],
-        ["wrath", "anger", "delight"],
-        ["diligent", "employed", "idle"],
-        ["outfit", "garb", "NONE"],
-        ["guide", "usher", "follow"],
+        ['wrath', 'anger', 'delight'],
+        ['diligent', 'employed', 'idle'],
+        ['fond', 'enamored', 'averse'],
+        ['outfit', 'garb', None],
+        ['guide', 'usher', 'follow'],
+    ]
+    actual = left_join(synonyms, antonyms)
+    assert actual == expected
+
+
+def test_fail():
+    synonyms = Hashtable()
+    synonyms.set("diligent", "employed")
+    synonyms.set("fond", "enamored")
+    synonyms.set("guide", "usher")
+    synonyms.set("outfit", "garb")
+    synonyms.set("wrath", "anger")
+
+    antonyms = Hashtable()
+    antonyms.set("diligent", "idle")
+    antonyms.set("fond", "averse")
+    antonyms.set("guide", "follow")
+    antonyms.set("flow", "jam")
+    antonyms.set("wrath", "delight")
+
+    expected = [
+        ['outfit', 'garb', None],
+        ['fond', 'enamored', 'averse'],
+        ['guide', 'usher', 'follow'],
+        ['diligent', 'employed', 'idle'],
+        ['wrath', 'anger', 'delight'],
+    ]
+    actual = left_join(synonyms, antonyms)
+    assert actual != expected
+
+
+def test_uneven_buckets():
+    synonyms = Hashtable()
+    synonyms.set("diligent", "employed")
+    synonyms.set("fond", "enamored")
+    synonyms.set("guide", "usher")
+    synonyms.set("outfit", "garb")
+    synonyms.set("wrath", "anger")
+
+    antonyms = Hashtable()
+    antonyms.set("diligent", "idle")
+    antonyms.set("fond", "averse")
+
+    expected = [
+        ['wrath', 'anger', None],
+        ['diligent', 'employed', 'idle'],
+        ['fond', 'enamored', 'averse'],
+        ['outfit', 'garb', None],
+        ['guide', 'usher', None],
     ]
 
     actual = left_join(synonyms, antonyms)
-
     assert actual == expected
